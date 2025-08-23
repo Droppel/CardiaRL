ad8416cb1f1847b66d61ef9cfa98eb2a1d997435
DoppelRR <leagueoflegends@sofortstart.de>
2025-08-23 18:39:59 +0200

    Initial commit

---
+++ cardiasim/cards.py
@@ -0,0 +1,325 @@
+import typing
+import random
+if typing.TYPE_CHECKING:
+    from game import Game, Player, Encounter
+
+class Card():
+    def __init__(self):
+        self.name = "default"
+        self.influence = -1
+        self.faction = -1 # -1 Nothing, 0 Green, 1 Yellow, 2 Red, 3 Blue
+        pass
+
+    def effect(self, game: 'Game', player_id: 'int'):
+        print(f"Activate {self.name} effect")
+
+class HiredBlade(Card):
+    def __init__(self):
+        super().__init__()
+        self.name = "Hired Blade"
+        self.influence = 1
+        self.faction = 0
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        current_encounter = game.encounters[game.current_encounter]
+        # Discard the played cards
+        game.players[0].discard.append(current_encounter.cards[0])
+        game.players[1].discard.append(current_encounter.cards[1])
+        # Remove the encounter from the game
+        game.encounters.remove(current_encounter)
+
+class VoidmageChoice():
+    def __init__(self, encounter, modOrPerm, player):
+        self.encounter = encounter
+        self.modOrPerm = modOrPerm # True for mod, False for permanent
+        self.player = player
+    
+    def __str__(self):
+        return f"Enc {self.encounter}: {'Mod' if self.modOrPerm else 'Perm'}"
+
+class Voidmage(Card):
+    def __init__(self):
+        super().__init__()
+        self.name = "Voidmage"
+        self.influence = 2
+        self.faction = 1
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        options = []
+        count = 0
+        for enc in game.encounters:
+            if enc.permanent[0]:
+                options.append(VoidmageChoice(count, False, 0))
+            if enc.permanent[1]:
+                options.append(VoidmageChoice(count, False, 1))
+            if enc.modifiers[0] != 0:
+                options.append(VoidmageChoice(count, True, 0))
+            if enc.modifiers[1] != 0:
+                options.append(VoidmageChoice(count, True, 1))
+            count += 1
+
+        if not options:
+            return
+        choice: VoidmageChoice = game.players[player_id].choose(options)
+        encounter = game.encounters[choice.encounter]
+        if choice.modOrPerm:
+            encounter.modifiers[choice.player] = 0
+        else:
+            encounter.permanent[choice.player] = False
+        print(f"Removed {'modifier' if choice.modOrPerm else 'permanent'} from encounter {choice.encounter} for player {choice.player}")
+
+class Surgeon(Card):
+    def __init__(self):
+        self.name = "Surgeon"
+        self.influence = 3
+        self.faction = 2
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Surgeon effect")
+        next_encounter = game.get_encounter(game.current_encounter + 1)
+        next_encounter.modifiers[player_id] += 5
+
+class Mediator(Card):
+    def __init__(self):
+        self.name = "Mediator"
+        self.influence = 4
+        self.faction = 3
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Mediator effect")
+        game.encounters[game.current_encounter].permanent[player_id] = True
+
+class Saboteur(Card):
+    def __init__(self):
+        self.name = "Saboteur"
+        self.influence = 5
+        self.faction = 0
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Saboteur effect")
+        other_player_id = player_id ^ 1
+        card1 = game.players[other_player_id].deck.pop()
+        card2 = game.players[other_player_id].deck.pop()
+        game.players[other_player_id].discard.append(card1)
+        game.players[other_player_id].discard.append(card2)
+        print(f"{game.players[other_player_id].name} discards {card1.name} and {card2.name} from the top of their deck")
+
+class FortuneTeller(Card):
+    def __init__(self):
+        self.name = "Fortune Teller"
+        self.influence = 6
+        self.faction = 1
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Fortune Teller effect")
+        other_player_id = player_id ^ 1
+        game.playopen[other_player_id] = True
+
+class PalaceGuard(Card):
+    def __init__(self):
+        self.name = "Palace Guard"
+        self.influence = 7
+        self.faction = 2
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Palace Guard effect")
+        other_player_id = player_id ^ 1
+        other_player = game.players[other_player_id]
+        player = game.players[player_id]
+        
+        options = [0, 1, 2, 3]
+        choice = player.choose(options)
+        
+        options = [card for card in other_player.hand if card.faction == choice]
+
+        hasdiscarded = False
+        # If player has cards matching the faction give option to discard one
+        if options:
+            options.append(None) # Option to not discard
+            discard_choice = other_player.choose(options)
+            if discard_choice:
+                other_player.hand.remove(discard_choice)
+                other_player.discard.append(discard_choice)
+                print(f"{other_player.name} discards {discard_choice.name}")
+                hasdiscarded = True
+        
+        if not hasdiscarded:
+            print(f"{other_player.name} does not discard a card.")
+            game.encounters[game.current_encounter].modifiers[player_id] += 7
+
+class Judge(Card):
+    def __init__(self):
+        self.name = "Judge"
+        self.influence = 8
+        self.faction = 3
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Judge effect")
+        encounter = game.encounters[game.current_encounter]
+        encounter.permanent[player_id] = True
+
+class Ambusher(Card):
+    def __init__(self):
+        self.name = "Ambusher"
+        self.influence = 9
+        self.faction = 0
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Ambusher effect")
+        options = [0, 1, 2, 3]
+        player = game.players[player_id]
+        choice = player.choose(options)
+        print(f"{player.name} chooses faction {choice} for opponent to discard")
+
+        other_player_id = player_id ^ 1
+        other_player = game.players[other_player_id]
+
+        for card in other_player.hand:
+            if card.faction == choice:
+                other_player.hand.remove(card)
+                other_player.discard.append(card)
+                print(f"{other_player.name} discards {card.name}")
+
+class Puppeteer(Card):
+    def __init__(self):
+        self.name = "Puppeteer"
+        self.influence = 10
+        self.faction = 1
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Puppeteer effect")
+        encounter = game.encounters[game.current_encounter]
+        player = game.players[player_id]
+        other_player_id = player_id ^ 1
+        other_player = game.players[other_player_id]
+
+        other_player.discard.append(encounter.cards[other_player_id])
+        encounter.cards[other_player_id] = None
+        newcard = random.choice(other_player.hand)
+        encounter.cards[other_player_id] = newcard
+        other_player.hand.remove(newcard)
+
+class Clockmaker(Card):
+    def __init__(self):
+        self.name = "Clockmaker"
+        self.influence = 11
+        self.faction = 2
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Clockmaker effect")
+        prev_encounter = game.encounters[game.current_encounter - 1] if game.current_encounter > 0 else None
+        if prev_encounter:
+            prev_encounter.modifiers[player_id] += 3
+
+        next_encounter = game.get_encounter(game.current_encounter + 1)
+        next_encounter.modifiers[player_id] += 3
+
+class Treasurer(Card):
+    def __init__(self):
+        self.name = "Treasurer"
+        self.influence = 12
+        self.faction = 3
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Treasurer effect")
+        encounter = game.encounters[game.current_encounter]
+        encounter.permanent[player_id] = True
+
+class SwampGuardian(Card):
+    def __init__(self):
+        self.name = "Swamp Guardian"
+        self.influence = 13
+        self.faction = 0
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Swamp Guardian effect")
+        player = game.players[player_id]
+        other_player_id = player_id ^ 1
+        other_player = game.players[other_player_id]
+        # Get available encounters
+        options = range(0, game.current_encounter-1)
+        if not options:
+            return
+        choice = player.choose(options)
+        encounter: Encounter = game.encounters[choice]
+        # Give player card back, discard other player's card
+        player.hand.append(encounter.cards[player_id])
+        other_player.discard.append(encounter.cards[other_player_id])
+        # Remove the encounter from the game
+        game.encounters.remove(encounter)
+
+class Magistra(Card):
+    def __init__(self):
+        self.name = "Magistra"
+        self.influence = 14
+        self.faction = 1
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Magistra effect")
+        mininfl = game.encounters[game.current_encounter].cards[player_id].influence
+        options: typing.List[Card] = []
+        option_names: typing.List[str] = []
+        for enc in game.encounters:
+            if enc.cards[player_id].influence > mininfl:
+                options.append(enc.cards[player_id])
+                option_names.append(enc.cards[player_id].name)
+        if not options:
+            return
+        player = game.players[player_id]
+        choice = player.choose(option_names)
+        if choice:
+            card = options[option_names.index(choice)]
+            card.effect(game, player)
+
+class InventorChoice():
+    def __init__(self, enc, player_id):
+        self.encounter: Encounter = enc
+        self.player_id: int = player_id
+
+class Inventor(Card):
+    def __init__(self):
+        self.name = "Inventor"
+        self.influence = 15
+        self.faction = 2
+    
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Inventor effect")
+        player = game.players[player_id]
+        other_player_id = player_id ^ 1
+
+        options: typing.List[InventorChoice] = []
+        for enc in game.encounters:
+            options.append(InventorChoice(enc, 0))
+            options.append(InventorChoice(enc, 1))
+
+        inc_choice: InventorChoice = player.choose(options)
+        inc_choice.encounter.modifiers[inc_choice.player_id] += 3
+
+        dec_choice: InventorChoice = player.choose(options)
+        dec_choice.encounter.modifiers[dec_choice.player_id] -= 3
+
+class Djinn(Card):
+    def __init__(self):
+        self.name = "Djinn"
+        self.influence = 16
+        self.faction = 3
+
+    def effect(self, game: 'Game', player_id: 'int'):
+        super().effect(game, player_id)
+        print(f"Activate Djinn effect")
+        game.winner = player_id
---
+++ cardiasim/game.py
@@ -0,0 +1,255 @@
+import typing
+import random
+
+from cards import *
+
+class Encounter():
+    def __init__(self):
+        self.cards: typing.List[Card] = [None, None]
+        self.permanent: typing.List[bool] = [False, False]
+        self.modifiers: typing.List[int] = [0, 0]
+
+    def get_sigil(self, draw_winner):
+        if self.cards[0] is None or self.cards[1] is None:
+            return -1 # Not possible to evaluate
+        if self.cards[0].name == "Mediator" and self.permanent[0]:
+            return draw_winner
+        if self.cards[1].name == "Mediator" and self.permanent[1]:
+            return draw_winner
+
+        if self.cards[0].influence + self.modifiers[0] > self.cards[1].influence + self.modifiers[1]:
+            return 1
+        elif self.cards[0].influence + self.modifiers[0] < self.cards[1].influence + self.modifiers[1]:
+            return 2
+
+        return draw_winner  # Tie
+
+    def __str__(self):
+        return f" - {self.cards[0].name} ({self.cards[0].influence+self.modifiers[0]}) vs {self.cards[1].name} ({self.cards[1].influence+self.modifiers[1]})"
+
+class Game():
+    def __init__(self):
+        self.players: typing.List[Player] = []
+        self.players.append(CPU("Cpu", 0))
+        self.players.append(Human("Human", 1))
+
+        self.playopen = [False, False]
+        self.current_encounter = 0
+        self.encounters: typing.List[Encounter] = []
+        self.drawwinner = 0  # 0 for draw, 1 for player1, 2 for player2, 3 for both
+        self.winner = 0
+        pass
+
+    def get_encounter(self, index) -> Encounter:
+        if index < len(self.encounters):
+            return self.encounters[index]
+        new_enc = Encounter()
+        self.encounters.append(new_enc)
+        return new_enc
+
+    def print(self):
+        print(f"{self.players[0].name} Handcards: {len(self.players[0].hand)}")
+        print(f"{self.players[0].name} Deck: {len(self.players[0].deck)}")
+        player1_discard = f"{self.players[0].name} Discard: "
+        for card in self.players[0].discard:
+            player1_discard += card.name + ", "
+        print(player1_discard)
+        print("-----")
+        print(f"{self.players[1].name} Handcards: {len(self.players[1].hand)}")
+        print(f"{self.players[1].name} Deck: {len(self.players[1].deck)}")
+        player2_discard = f"{self.players[1].name} Discard: "
+        for card in self.players[1].discard:
+            player2_discard += card.name + ", "
+        print(player2_discard)
+        print("-----")
+        print("Encounters:")
+        for encounter in self.encounters:
+            if encounter.cards[0] and encounter.cards[1]:
+                print(encounter)
+        print("-----")
+
+    def turn(self):
+        print("Starting turn")
+        self.print()
+        self.current_encounter = len(self.encounters)
+
+        player1_card: Card = None
+        player2_card: Card = None
+        if self.playopen[0]:
+            player1_card = self.players[0].play_card()
+            print(f"{self.players[0].name} played {player1_card.name}")
+            player2_card = self.players[1].play_card()
+        elif self.playopen[1]:
+            player2_card = self.players[1].play_card()
+            print(f"{self.players[1].name} played {player2_card.name}")
+            player1_card = self.players[0].play_card()
+        else:
+            player1_card = self.players[0].play_card()
+            player2_card = self.players[1].play_card()
+
+        print(f"Player 1 played: {player1_card.name if player1_card else 'No card'}")
+        print(f"Player 2 played: {player2_card.name if player2_card else 'No card'}")
+        if player1_card is None and player2_card is None:
+            print("No cards left! The game ends in a draw.")
+            return
+        if player1_card is None:
+            print(f"{self.players[0].name} has no cards to play. Player 2 wins!")
+            return
+        if player2_card is None:
+            print(f"{self.players[1].name} has no cards to play. Player 1 wins!")
+            return
+        
+        # Reset playopen states
+        self.playopen = [False, False]
+        
+        # Play cards
+        print(f"{self.players[0].name} plays {player1_card.name}: Influence {player1_card.influence}")
+        print(f"{self.players[1].name} plays {player2_card.name}: Influence {player2_card.influence}")
+        encounter = self.get_encounter(self.current_encounter)
+        encounter.cards[0] = player1_card
+        encounter.cards[1] = player2_card
+        sigil = encounter.get_sigil(self.drawwinner)
+        if sigil == -1:
+            print("Not possible to evaluate sigil.")
+        elif sigil == 0:
+            print("Both players played cards with equal influence. No sigils awarded.")
+        elif sigil == 1:
+            player2_card.effect(self, self.players[1].id)
+        else:
+            player1_card.effect(self, self.players[0].id)
+
+        if game.winner != 0:
+            print(f"Player {game.winner} has already won the game.")
+            return
+
+        self.players[0].draw_card()
+        self.players[1].draw_card()
+
+        # Check winner
+        player1_sigils, player2_sigils = 0, 0
+        # Check Judge
+        self.drawwinner = 0
+        for enc in self.encounters:
+            if enc.cards[0] and enc.cards[0].name == "Judge" and enc.permanent[0]:
+                self.drawwinner += 1
+            if enc.cards[1] and enc.cards[1].name == "Judge" and enc.permanent[1]:
+                self.drawwinner += 2
+
+        treasurer_bonus = 0
+        for enc in reversed(self.encounters):
+            winner = enc.get_sigil(self.drawwinner)
+            if winner == 1:
+                player1_sigils += 1 + treasurer_bonus
+            elif winner == 2:
+                player2_sigils += 1 + treasurer_bonus
+            elif winner == 3:
+                player1_sigils += 1 + treasurer_bonus
+                player2_sigils += 1 + treasurer_bonus
+            treasurer_bonus = 0
+            if enc.cards[0] and enc.cards[0].name == "Treasurer" and enc.permanent[0]:
+                treasurer_bonus += 1
+            if enc.cards[1] and enc.cards[1].name == "Treasurer" and enc.permanent[1]:
+                treasurer_bonus += 1
+        
+        if player1_sigils == player2_sigils:
+            pass
+        elif player1_sigils >= 5 and player1_sigils > player2_sigils:
+            print(f"{self.players[0].name} wins!")
+            return
+        elif player2_sigils >= 5 and player2_sigils > player1_sigils:
+            print(f"{self.players[1].name} wins!")
+            return
+
+        print(f"End of turn. Sigils - {self.players[0].name}: {player1_sigils}, {self.players[1].name}: {player2_sigils}")
+
+        self.turn()
+
+class Player():
+    deck:  typing.List[Card]
+    hand: typing.List[Card]
+    discard: typing.List[Card] 
+    name: str
+    id: int
+
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.hand = []
+        self.deck = []
+        self.discard = []
+        self.setup_deck()
+        self.draw_card(5)
+
+    def setup_deck(self):
+        # Dynamically import all Card subclasses from py
+        self.deck = [HiredBlade(), Voidmage(), Surgeon(), Mediator(), Saboteur(),
+                     FortuneTeller(), PalaceGuard(), Judge(), Ambusher(), Puppeteer(),
+                     Clockmaker(), Treasurer(), SwampGuardian(), Magistra(), Inventor(),
+                     Djinn()]
+        random.shuffle(self.deck)
+
+    def draw_card(self, count = 1):
+        for _ in range(count):
+            if len(self.deck) > 0:
+                card = self.deck.pop(0)
+                self.hand.append(card)
+
+    def print_hand(self):
+        print(f"{self.name}'s hand:")
+        for card in self.hand:
+            print(f"- {card.name}")
+
+    def choose(self, choice_list):
+        pass
+
+    def play_card(self):
+        pass
+
+class CPU(Player):
+    def __init__(self, name, id):
+        super().__init__(name, id)
+
+    def choose(self, choice_list):
+        return random.choice(choice_list)
+
+    def play_card(self) -> Card:
+        if self.hand:
+            card = random.choice(self.hand)
+            self.hand.remove(card)
+            return card
+        else:
+            print(f"{self.name} has no cards to play.")
+            return None
+
+class Human(Player):
+    def __init__(self, name, id):
+        super().__init__(name, id)
+
+    def choose(self, choice_list):
+        print("Available choices:")
+        for i, choice in enumerate(choice_list):
+            print(f"{i + 1}: {choice}")
+
+        choice_index = int(input("Choose an option: ")) - 1
+        if 0 <= choice_index < len(choice_list):
+            return choice_list[choice_index]
+        return choice_list[0]
+
+    def play_card(self) -> Card:
+        if self.hand:
+            self.print_hand()
+            card = None
+            cardname = input("Enter card to play:")
+            if cardname in [c.name for c in self.hand]:
+                card = self.hand[[c.name for c in self.hand].index(cardname)]
+            else:
+                card = self.hand[0]  # For simplicity, just play the first card in hand
+            self.hand.remove(card)
+            return card
+        else:
+            print(f"{self.name} has no cards to play.")
+            return None
+
+if __name__ == "__main__":
+    game = Game()
+    game.turn()  # Start the game
